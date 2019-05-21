d142a77889ad4f2bde41a295007eefa8ec5c3ac0
LeenSamZiv <1179364851@qq.com>
2019-05-21 16:44:30 +0800

    update bat

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-cd web && npm run build && cd ../server && mvn clean package && cd src/main/resources/ && rmdir /s static /q
+cd web && npm run build && cd ../server && mvn clean package && cd src/main/resources/ && rmdir /s static /q && cd ../../../../
