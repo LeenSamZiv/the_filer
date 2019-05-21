5a20e79b71ae2e9dd66aced8e81a32c58a27df84
LeenSamZiv <1179364851@qq.com>
2019-05-21 16:37:12 +0800

    update bat&README

---
+++ build.bat
@@ -0,0 +1,1 @@
+cd web && npm run build && cd ../server && mvn clean package && cd src/main/resources/ && rmdir /s static /q
---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+for /f "tokens=4" %%a in ('route print^|findstr 0.0.0.0.*0.0.0.0') do (
+ set IP=%%a
+)
+chcp 65001
+
+echo 请访问 %IP%:8080 以上传文件
+start D:\_the_filer_upload
+java -jar server\target\the_filer-1.2.jar
