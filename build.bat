2e51fff75efa69f36e89f308f6d4b6e7e2910f08
LeenSamZiv <1179364851@qq.com>
2019-05-28 10:06:44 +0800

    update build script

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-cd web && npm run build && cd ../server && mvn clean package && cd src/main/resources/ && rmdir /s static /q && cd ../../../../
+cd web && npm install && npm run build && cd ../server && mvn clean package && cd src/main/resources/ && rmdir /s static /q && cd ../../../../
