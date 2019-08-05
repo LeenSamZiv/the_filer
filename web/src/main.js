8c0a1c9e94d31fecc44ec7e37d14598498b9ea09
LeenSamZiv <1179364851@qq.com>
2019-08-05 09:03:39 +0800

    🔧 known issues

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+const isDebug_mode = process.env.NODE_ENV !== 'production';
+Vue.config.debug = isDebug_mode;
+Vue.config.devtools = isDebug_mode;
+Vue.config.productionTip = isDebug_mode;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: h => h(App),
+}).$mount('#app');
